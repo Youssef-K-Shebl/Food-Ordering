07631ee51455a81af18ac239bcb294cf113d925e
Youssef Kamel <89489352+Youssef-K-Shebl@users.noreply.github.com>
2022-02-15 18:42:47 +0200

    Config File Updated

--- config/conn.php
+++ config/conn.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-const SITEURL = 'alexrestaurant.epizy.com';
-const LOCALHOST = 'sql207.epizy.com';
-const USERNAME = 'epiz_30986374';
-const PASSWORD = 'RVvx6rlqoB4q';
-const DBNAME = 'epiz_30986374_FoodOrdering	';
+const SITEURL = 'https://localhost/foodOrderingProject/';
+const LOCALHOST = 'localhost';
+const USERNAME = 'root';
+const PASSWORD = '';
+const DBNAME = 'foodorderingproject';
 $conn = mysqli_connect(LOCALHOST, USERNAME, PASSWORD, DBNAME) or die(mysqli_error());
